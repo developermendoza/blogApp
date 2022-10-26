bf9d479d34b0a141f3b871cc19d5c0d5411f8dba
Jose Mendoza <developermendoza@gmail.com>
2022-10-26 13:45:07 -0700

    add test form to submit posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-// Import the functions you need from the SDKs you need
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs, getDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  addDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBGwtSUTJ_W9rO2QLcHWTfz76U9GOOPRi0",
@@ -19,61 +25,88 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+
+const initialStateNewPost = {
+  title: "",
+  body: "",
+};
 // const snapshot = await getDocs(postsCol);
 function App() {
   const [posts, setPosts] = useState([]);
-
-  // const data = async () => {
-  //   const snapshot = await getDocs(postsCol);
-  //   return snapshot;
-  // };
+  const [newPost, setNewPost] = useState(initialStateNewPost);
 
   const getPosts = async (db) => {
     const postsCol = collection(db, "posts");
     const postSnapshot = await getDocs(postsCol);
-    const postList = postSnapshot.docs.map((doc) => doc.data());
+    const postList = postSnapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
     setPosts(postList);
-    // console.log("postList: ", postList);
-    // return postList;
+  };
+
+  const addPost = async (post) => {
+    try {
+      const docRef = await addDoc(collection(db, "posts"), post);
+
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   };
 
   useEffect(() => {
     getPosts(db);
-    // const data = getCities(db);
-    // console.log("data: ", data);
-    // declare the data fetching function
-    // const fetchData = async () => {
-    //   const data = await getDocs(postsCol);
-    //   console.log("data: ", data);
-    // };
-    // fetchData();
-    // call the function
-    // fetchData()
-    // console.log();
-    // make sure to catch any error
-    // .catch(console.error);
-    // const snapshot = await getDocs(postsCol);
-    // const getData = data;
-    // console.log("getData: ", getData);
-    //  async ()=>{
-    //  }
-    // fetch("http://localhost:8080/")
-    //   .then((response) => {
-    //     if (response.ok) {
-    //       return response.json();
-    //     }
-    //     throw response;
-    //   })
-    //   .then((data) => setGetPosts(data))
-    //   .catch((err) => console.log("error: ", err));
   }, []);
+
+  const handlePostChange = (e) => {
+    const { name, value } = e.target;
+
+    setNewPost({
+      ...newPost,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addPost(newPost);
+    setNewPost(initialStateNewPost);
+  };
+
   return (
     <div className="App">
-      <ul>
-        {posts.map((post) => (
-          <li key={post.title}>{post.title}</li>
-        ))}
-      </ul>
+      <Container>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="text"
+              placeholder="Post Title"
+              value={newPost.title}
+              name="title"
+              onChange={handlePostChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              as="textarea"
+              placeholder="Leave a post here"
+              style={{ height: "100px" }}
+              name="body"
+              value={newPost.body}
+              onChange={handlePostChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
