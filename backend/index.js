8158f89c41af1cecc41e8c6652653c812ae7e213
Jose Mendoza <developermendoza@gmail.com>
2022-10-26 07:04:11 -0700

    update backend.  setup firebase for backend

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,41 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { dbConnection } from "./db/conn.js";
 import { getPosts } from "./routes/posts.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const app = express();
-const port = process.env.PORT || 8080;
-app.use(cors());
-dotenv.config();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGwtSUTJ_W9rO2QLcHWTfz76U9GOOPRi0",
+  authDomain: "blogapp-48df7.firebaseapp.com",
+  projectId: "blogapp-48df7",
+  storageBucket: "blogapp-48df7.appspot.com",
+  messagingSenderId: "621784057473",
+  appId: "1:621784057473:web:46d83625f56dc39a5f6f80",
+  measurementId: "G-83WW8FDH0W",
+};
 
-dbConnection();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// create application/json parser
-const jsonParser = bodyParser.json();
+// const app = express();
+// const port = process.env.PORT || 8080;
+// app.use(cors());
+// dotenv.config();
 
-// create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// dbConnection();
 
-app.get("/", urlencodedParser, getPosts);
+// // create application/json parser
+// const jsonParser = bodyParser.json();
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+// // create application/x-www-form-urlencoded parser
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// app.get("/", urlencodedParser, getPosts);
+
+// app.listen(port, () => console.log(`Server running on port: ${port}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,77 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, getDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGwtSUTJ_W9rO2QLcHWTfz76U9GOOPRi0",
+  authDomain: "blogapp-48df7.firebaseapp.com",
+  projectId: "blogapp-48df7",
+  storageBucket: "blogapp-48df7.appspot.com",
+  messagingSenderId: "621784057473",
+  appId: "1:621784057473:web:46d83625f56dc39a5f6f80",
+  measurementId: "G-83WW8FDH0W",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+// const snapshot = await getDocs(postsCol);
 function App() {
-  const [getPosts, setGetPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  // const data = async () => {
+  //   const snapshot = await getDocs(postsCol);
+  //   return snapshot;
+  // };
+
+  const getPosts = async (db) => {
+    const postsCol = collection(db, "posts");
+    const postSnapshot = await getDocs(postsCol);
+    const postList = postSnapshot.docs.map((doc) => doc.data());
+    setPosts(postList);
+    // console.log("postList: ", postList);
+    // return postList;
+  };
 
   useEffect(() => {
-    fetch("http://localhost:8080/")
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw response;
-      })
-      .then((data) => setGetPosts(data))
-      .catch((err) => console.log("error: ", err));
+    getPosts(db);
+    // const data = getCities(db);
+    // console.log("data: ", data);
+    // declare the data fetching function
+    // const fetchData = async () => {
+    //   const data = await getDocs(postsCol);
+    //   console.log("data: ", data);
+    // };
+    // fetchData();
+    // call the function
+    // fetchData()
+    // console.log();
+    // make sure to catch any error
+    // .catch(console.error);
+    // const snapshot = await getDocs(postsCol);
+    // const getData = data;
+    // console.log("getData: ", getData);
+    //  async ()=>{
+    //  }
+    // fetch("http://localhost:8080/")
+    //   .then((response) => {
+    //     if (response.ok) {
+    //       return response.json();
+    //     }
+    //     throw response;
+    //   })
+    //   .then((data) => setGetPosts(data))
+    //   .catch((err) => console.log("error: ", err));
   }, []);
-
   return (
     <div className="App">
       <ul>
-        {getPosts.map((post) => (
-          <li key={post._id}>{post.title}</li>
+        {posts.map((post) => (
+          <li key={post.title}>{post.title}</li>
         ))}
       </ul>
     </div>
